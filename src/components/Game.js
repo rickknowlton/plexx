faf31381522a6a252c6dae43099c60f131473a17
rickknowlton <richard.d.knowlton@gmail.com>
2018-11-05 19:13:50 -0500

    React Site

---
+++ src/components/Game.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Row, Column, Card } from '../m-components';
+import '../css/game.css';
+import game from '../img/game.png';
+
+const Game = props => (
+    <React.Fragment>
+    <div className="card center grey darken-4 game z-depth-5">
+      {/* <h2>[Insert Game Here]</h2> */}
+      <div className="game-content darken-4 z-depth-5">
+      <img className="responsive-img img" src={game}></img>
+      </div>
+    </div>
+    </React.Fragment>
+);
+
+export default Game;
---
+++ src/m-components/Card.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export const Card = props => (
+  <div className="card grey darken-4">
+    <div className="card-content white-text flow-text">
+    </div>
+  </div>
+);
