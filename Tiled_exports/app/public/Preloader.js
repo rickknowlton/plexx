b0cb7b3d38a8928f469cfd48344a0de4e000f05a
row_gregory <rowgregory@gmail.com>
2018-11-28 20:40:59 -0500

    second commit

--- Tiled_exports/app/public/Preloader.js
+++ Tiled_exports/app/public/Preloader.js
@@ -18,16 +18,34 @@ Game.Preloader.prototype = {
 
         // load assets
 
-        this.load.tilemap('map', '/images/Level1.csv' );
+
+
+        this.load.tilemap('map', '/images/new_level_layout.csv' );
 
        
-        this.load.image('tileset','/images/completeTileset.png');
+        this.load.image('tileset','/images/new_tileset.png');
 
         this.load.spritesheet('bad_dude','/images/bad_dude.png', 32, 48);
 
+        this.load.spritesheet('monkey','/images/monkey.png', 32, 32);
+
         this.load.image('yeti', '/images/yeti.png', 32, 32);
 
-        this.load.image('bullet', '/images/bullet.png');
+        this.load.image('background', '/images/background.png');
+
+        this.load.image('banana', '/images/banana.png');
+
+        this.load.spritesheet('coin', '/images/coin.png', 23, 24);
+
+        this.load.image('ladder', '/images/ladder.png');
+
+        this.load.image('spring', '/images/spring.png', 32, 32);
+
+        this.load.image('ground', '/images/platform.png');
+
+        this.load.image('ground2', '/images/platform2.png');
+
+        this.load.image('endZone', '/images/endZone.png');
 
 
     },
